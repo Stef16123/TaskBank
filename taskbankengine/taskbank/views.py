c67a4b28271e6c7de80febd3137aa56583d87a53
Stef16123 <m156e1i9v9r@yandex.ru>
2020-03-24 22:12:03 +0300

    Delete: ListView, added: search

--- taskbankengine/taskbank/views.py
+++ taskbankengine/taskbank/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 # from django.shortcuts import get_object_or_404
-# from django.core.paginator import Paginator
+from django.core.paginator import Paginator
 from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-
+# from django.views.generic.list import ListView
+from django.views.generic import View
+from django.db.models import Q
 
 from .models import Task
 
@@ -16,9 +17,23 @@ class TaskDetailView(DetailView):
 	template_name = 'taskbank/task_detail.html'
 
 
-class TasksListView(ListView):
+class TasksListView(View):
 	"""Обработка одной конкретной задачи: """
+
 	model = Task
-	paginate_by = 2
 	template_name = 'taskbank/index.html'
-	context_object_name = 'tasks'
+
+	def get(self,request):
+		search_query = request.GET.get('search', '')
+		if search_query:
+			task = self.model.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+		else:
+			task = self.model.objects.all()
+
+		paginator = Paginator(task, 2)
+		page_number = request.GET.get('page', 1)
+		page = paginator.get_page(page_number)
+		context = {
+		'tasks' : page,
+		}
+		return render(request, self.template_name, context)
